b6d573f246df48c00076a2718859eb5a9f530865
mobina <mobina.abbasi7849@gmail.com>
2022-09-04 15:44:18 +0430

    add final section to career page

---
+++ assets/js/Career_Opportunities.js
@@ -0,0 +1,45 @@
+tabcontent = document.getElementsByClassName("tabcontent");
+tabcontent[0].style.display = "flex";
+tabcontent[1].style.display = "none";
+tabcontent[2].style.display = "none";
+tabcontent[3].style.display = "none";
+function openDiv(evt, divName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+   document.getElementById(divName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+////////////////////////////////////////////////////
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("Slides");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}    
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";  
+}
+for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+}
